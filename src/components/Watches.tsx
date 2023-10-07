365e1cf71c090dafaebd988b27ba9669ca73aaa1
Liaksej <tarusov.liaksej@gmail.com>
2023-10-07 19:25:44 +0200

    Add delete function to Watches component
    Included a delete button into Watches component to enable removal of individual watches. Modified 'Watches.tsx' file to include IconButton from MUI library and Cancel icon.
    
    This added functionality enhances the component's usability by allowing users to delete specific watches as needed. Moreover, it improves the layout of the Watches component by adopting a 'flex' container to encapsulate the delete icon and watch display, rather than integrating the delete button directly into the watch component.
    
    Changes were made to both the structure of the Watches component and its visual presentation, contributing to an overall more user-friendly aspect of the application.

--- src/components/Watches.tsx
+++ src/components/Watches.tsx
@@ -1,5 +1,7 @@
 import { Action } from "@/app/page";
 import { Dispatch } from "react";
+import { IconButton } from "@mui/material";
+import Cancel from "@mui/icons-material/Cancel";
 
 interface WatchesProps {
   watch: {
@@ -12,10 +14,18 @@ interface WatchesProps {
 
 export const Watches = ({ watch, dispatch }: WatchesProps) => {
   return (
-    <div className="clock w-[100px] h-[100px] border-[1px] border-gray-500 border-solid relative rounded-full">
-      <div className="hand hour-hand w-1/2 bg-gray-700 origin-bottom rotate-90 h-[3px] left-[23%]"></div>
-      <div className="hand minute-hand w-1/2 bg-gray-700 origin-bottom rotate-[95deg] h-[2px] left-[26%]"></div>
-      <div className="hand second-hand w-1/2 bg-gray-700 origin-bottom rotate-[100deg] h-[1px] left-[29%]"></div>
+    <div className="flex flex-col">
+      <IconButton
+        className="abs h-[20px] w-[20px] self-end"
+        aria-label="delete"
+      >
+        <Cancel color="disabled" />
+      </IconButton>
+      <div className="clock w-[100px] h-[100px] border-[1px] border-gray-500 border-solid relative rounded-full">
+        <div className="hand hour-hand w-1/2 bg-gray-700 origin-bottom rotate-90 h-[3px] left-[23%]"></div>
+        <div className="hand minute-hand w-1/2 bg-gray-700 origin-bottom rotate-[95deg] h-[2px] left-[26%]"></div>
+        <div className="hand second-hand w-1/2 bg-gray-700 origin-bottom rotate-[100deg] h-[1px] left-[29%]"></div>
+      </div>
     </div>
   );
 };
