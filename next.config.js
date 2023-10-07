7a806d3ed36ce6ef09b5b8e29e7193a504244d40
Liaksej <tarusov.liaksej@gmail.com>
2023-10-07 19:16:08 +0200

    Update README and add new dependencies
    Updated content in README.md file for better project description. Also added new npm dependencies ('@emotion/react', '@emotion/styled', '@mui/material') necessary for Material-UI integration as well as various related packages for compatibility.
    
    The README.md was updated to better reflect the current state of the project, previous information was deprecated.
    
    New dependencies are needed to successfully integrate Material-UI for improved UI/UX. They were added to pnpm's lockfile manifest following package installation. Material-UI will provide pre-built components and design consistency across the project. Also added sub-dependencies, which were installed automatically as they are required by the new main dependencies.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/ra-lifecycle-http-watches" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
